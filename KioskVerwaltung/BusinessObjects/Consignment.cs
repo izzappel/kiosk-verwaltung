96737f6cb386dda11711b1cb86e5a51556745178
izzappel <izzappel@users.noreply.github.com>
2017-04-29 10:28:05 +0200

    Add files via upload

---
+++ KioskVerwaltung/BusinessObjects/Consignment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KioskVerwaltung.BusinessObjects
+{
+    public class Consignment
+    {
+        public int Id { get; set; }
+
+        public int NumberOfContent { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public String ExpirationDateString 
+        {
+            get 
+            { 
+                if(ExpirationDate.Equals(DateTime.MinValue)) 
+                {
+                    return "kein Ablaufdatum";
+                }
+                return ExpirationDate.ToShortDateString(); 
+            }
+        }
+        public double Price { get; set; }
+        
+        public Consignment()
+        {
+        }
+
+        public Consignment(int id, int numberOfContent, DateTime expirationDate, double price)
+        {
+            this.Id = id;
+            this.NumberOfContent = numberOfContent;
+            this.ExpirationDate = expirationDate;
+            this.Price = price;
+        }
+
+    }
+}
---
+++ KioskVerwaltung/Converters/SaleProductToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using KioskVerwaltung.BusinessObjects;
+using System.Windows.Media;
+
+namespace KioskVerwaltung.Converters
+{
+    public class SaleProductToColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            SaleProduct product = values[0] as SaleProduct;
+            if (product != null)
+            {
+                if (product.IsPrivate) { return new SolidColorBrush(Colors.Green); }
+                if (product.IsPaidByCreditCard) { return new SolidColorBrush(Colors.Blue); }
+            }
+            return new SolidColorBrush(Colors.Black); 
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
